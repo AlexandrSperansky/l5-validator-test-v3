810ce1f05cfb6812fa580a7c3e05ab1f3ff71b51
AlexandrSperansky <kopobka010@gmail.com>
2023-11-20 11:01:59 +0400

    final

---
+++ NumberSchema.js
@@ -0,0 +1,17 @@
+export default class Numberschema {
+    validators = [(value) => typeof value === 'number'];
+  
+    even() {
+      this.validators.push((value) => value % 2 === 0);
+      return this;
+    }
+  
+    odd() {
+      this.validators.push((value) => value % 2 !== 0);
+      return this;
+    }
+  
+    isValid(value) {
+      return this.validators.every((validator) => validator(value) === true);
+    }
+  }
